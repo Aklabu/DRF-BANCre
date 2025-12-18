9b5a1de93fc75891234e14a512730d517612752a
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2025-12-19 03:38:10 +0600

    fix few errors

--- accounts/models.py
+++ accounts/models.py
@@ -15,9 +15,16 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        # Set default values for required fields
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_verified', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
         return self.create_user(email, password, **extra_fields)
 
 
@@ -56,7 +63,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'customer_type']
+    REQUIRED_FIELDS = []  # Only email and password needed for superuser
     
     def __str__(self):
         return self.email
