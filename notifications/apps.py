f728215a013a21de29bebdcab92650691de79f29
Md. Joni Akter Labu <mdjoniakterlabu@gmail.com>
2026-03-12 03:55:38 +0600

    register signal handlers to avoid duplicate firing

--- notifications/apps.py
+++ notifications/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class NotificationsConfig(AppConfig):
-    name = 'notifications'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name               = 'notifications'
+
+    def ready(self):
+        # Register all signal handlers — imported here to avoid duplicate firing
+        import notifications.signals
